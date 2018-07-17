e02537712f998fd40cf7479959a8efd74d6c9dd9
Daan <foifhjidhf@xd.com>
2018-07-17 19:45:50 +0200

    Ew whitespace

--- src/Database.js
+++ src/Database.js
@@ -7,15 +7,15 @@ class Database {
 		this.knex = require("knex")({
 			client: "mysql2",
 			connection: {
-				"host"    : Config.host,
+				"host": Config.host,
 				"database": Config.database,
-				"user"    : Config.username,
+				"user": Config.username,
 				"password": Config.password
 			}
 		})
 	}
 
-	updateData(id, data) { return this.updateColumn(id, "data", data)}
+	updateData(id, data) { return this.updateColumn(id, "data", data) }
 	getData() { return this.knex("penguins").first("*") }
 	updateRoom(id, room) { return this.updateColumn(id, "room", room) }
 	getPlayerByName(username) { return this.knex("penguins").first("*").where("username", username) }
@@ -23,7 +23,7 @@ class Database {
 	insertPlayer(username) { return this.knex("penguins").insert({ username: username }) }
 	drop(id) { return this.knex("penguins").where("id", id).del() }
 	dropAll() { return this.knex("penguins").where("id", ">", 100).del() }
-	updateColumn(id, column, value) { return this.knex("penguins").update(column, value).where("id", id).then(() => {}).catch((err) => {if (err) throw err})}
+	updateColumn(id, column, value) { return this.knex("penguins").update(column, value).where("id", id).then(() => { }).catch((err) => { if (err) throw err }) }
 }
 
 module.exports = Database
--- src/Server.js
+++ src/Server.js
@@ -1,15 +1,15 @@
 "use strict"
 
 const database = new (require("./Database"))
-const fastify  = require("fastify")()
-const path     = require("path")
+const fastify = require("fastify")()
+const path = require("path")
 
-const newOpts  = { schema: { body: { type: "object", properties: { n : { type: "string" }}}}}
-const joinOpts = { schema: { body: { type: "object", properties: { n : { type: "string" }, id: { type: "number" }, d: { type: "string" }}}}}
-const chatOpts = { schema: { body: { type: "object", properties: { id : { type: "number" }, d: { type: "string" }}}}}
-const dropOpts = { schema: { body: { type: "object", properties: { id : { type: "number" }}}}}
+const newOpts = { schema: { body: { type: "object", properties: { n: { type: "string" } } } } }
+const joinOpts = { schema: { body: { type: "object", properties: { n: { type: "string" }, id: { type: "number" }, d: { type: "string" } } } } }
+const chatOpts = { schema: { body: { type: "object", properties: { id: { type: "number" }, d: { type: "string" } } } } }
+const dropOpts = { schema: { body: { type: "object", properties: { id: { type: "number" } } } } }
 
-fastify.register(require("fastify-static"), {root: path.join(__dirname, "public"),prefix: "/"})
+fastify.register(require("fastify-static"), { root: path.join(__dirname, "public"), prefix: "/" })
 fastify.register(require("fastify-formbody"))
 fastify.register(require("fastify-helmet"))
 
@@ -26,12 +26,12 @@ fastify.post("/new.php", newOpts, async (req, res) => {
 				const id = penguin[0]
 				database.getPlayerByName(username).then(penguin => {
 					const poll = `&id=${id}&m=${penguin.mod}&e=0`
-					Logger({level: "outgoing", msg: "Outgoing data", extra: {data: poll, url: "/new.php"}})
+					Logger({ level: "outgoing", msg: "Outgoing data", extra: { data: poll, url: "/new.php" } })
 					return res.code(200).type("text/html").send(poll)
 				})
 			})
 		} else {
-            return res.code(200).type("text/html").send(`&e=2`)
+			return res.code(200).type("text/html").send(`&e=2`)
 		}
 	})
 })
@@ -41,12 +41,12 @@ fastify.post("/join.php", joinOpts, async (req, res) => {
 	const room = data.substr(0, 1)
 	const chat = data.substr(1, 99)
 	const id = req.body.id
-	Logger({level: "incoming", msg: "Incoming data", extra: {data: `${id}${data}`, url: "/join.php"}})
+	Logger({ level: "incoming", msg: "Incoming data", extra: { data: `${id}${data}`, url: "/join.php" } })
 	database.updateData(id, chat)
 	database.updateRoom(id, room)
 	database.getData().then(penguin => {
 		const poll = `&p=${penguin.room}${penguin.data.substr(1, 4)}${penguin.username}`
-		Logger({level: "outgoing", msg: "Outgoing data", extra: {data: poll, url: "/join.php"}})
+		Logger({ level: "outgoing", msg: "Outgoing data", extra: { data: poll, url: "/join.php" } })
 		return res.code(200).type("text/html").send(poll + "\r\n")
 	})
 })
@@ -55,58 +55,58 @@ fastify.post("/chat.php", chatOpts, async (req, res) => {
 	const data = req.body.d
 	const chat = data.substr(0, 99)
 	const id = req.body.id
-	Logger({level: "incoming", msg: "Incoming data", extra: {data: `${id}${data}`, url: "/chat.php"}})
-    if (data.length > 2) {
-    	database.updateData(id, data)
-    	database.getData().then(penguin => {
-    		const poll = `&c=${penguin.room}${penguin.data}`
-    		Logger({level: "outgoing", msg: "Outgoing data", extra: {data: poll, url: "/chat.php"}})
-    		return res.code(200).type("text/html").send(poll + "\r\n")
-    	})
-    } else {
-    	database.getData().then(penguin => {
-    		const poll = `&c=${penguin.room}${penguin.data}`
-    		Logger({level: "outgoing", msg: "Outgoing data", extra: {data: poll, url: "/chat.php"}})
-    		return res.code(200).type("text/html").send(poll + "\r\n")
-    	})
-    }
+	Logger({ level: "incoming", msg: "Incoming data", extra: { data: `${id}${data}`, url: "/chat.php" } })
+	if (data.length > 2) {
+		database.updateData(id, data)
+		database.getData().then(penguin => {
+			const poll = `&c=${penguin.room}${penguin.data}`
+			Logger({ level: "outgoing", msg: "Outgoing data", extra: { data: poll, url: "/chat.php" } })
+			return res.code(200).type("text/html").send(poll + "\r\n")
+		})
+	} else {
+		database.getData().then(penguin => {
+			const poll = `&c=${penguin.room}${penguin.data}`
+			Logger({ level: "outgoing", msg: "Outgoing data", extra: { data: poll, url: "/chat.php" } })
+			return res.code(200).type("text/html").send(poll + "\r\n")
+		})
+	}
 })
 
 fastify.post("/drop.php", dropOpts, async (req, res) => {
 	const id = req.body.id
 	database.drop(id).then(() => {
-		Logger({level: "info", msg: `Dropped ${id}`})
+		Logger({ level: "info", msg: `Dropped ${id}` })
 	}).catch((err) => {
-		Logger({level: "error", msg: err})
+		Logger({ level: "error", msg: err })
 	})
 })
 
 const Logger = (obj) => {
-	const toLog = JSON.stringify({level: obj.level, time: Date.now(), msg: obj.msg, extra: obj.extra || {}})
-	const stream = require("fs").createWriteStream(`${__dirname}\\logs\\${obj.level}.log`, {flags: "a"})
+	const toLog = JSON.stringify({ level: obj.level, time: Date.now(), msg: obj.msg, extra: obj.extra || {} })
+	const stream = require("fs").createWriteStream(`${__dirname}\\logs\\${obj.level}.log`, { flags: "a" })
 	stream.write(`${toLog}\r\n`)
 	return console.log(toLog)
 }
 
 const shutDown = () => {
-	Logger({level: "info", msg: "Server shutting down in 3 seconds..."})
+	Logger({ level: "info", msg: "Server shutting down in 3 seconds..." })
 	database.dropAll().then(() => {
-		Logger({level: "info", msg: `Cleaned the database`})
+		Logger({ level: "info", msg: `Cleaned the database` })
 	}).catch((err) => {
-		Logger({level: "error", msg: err})
+		Logger({ level: "error", msg: err })
 	})
 	setTimeout(() => { process.exit(0) }, 3000)
 }
 
 const start = async () => {
 	try {
-        await fastify.listen(80)
-        Logger({level: "info", msg: "Server listening on http://localhost:80/"})
-        process.on("SIGINT",  () => shutDown())
-        process.on("SIGTERM", () => shutDown())
+		await fastify.listen(80)
+		Logger({ level: "info", msg: "Server listening on http://localhost:80/" })
+		process.on("SIGINT", () => shutDown())
+		process.on("SIGTERM", () => shutDown())
 	} catch (err) {
-        Logger({level: "error", msg: err})
-        process.exit(1)
+		Logger({ level: "error", msg: err })
+		process.exit(1)
 	}
 }
 
